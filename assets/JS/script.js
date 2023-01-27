1d348972ff444c5a161be36c7a6aad737834e2e7
Stuart <stuart@ucreseller.co.uk>
2023-01-27 19:12:40 +0000

    Added JS code to display current date and time to page by targeting the ID CurrentDay

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+
+// Add the Current Time and Date to the Page.
+
+    var currentDayEl = $('#currentDay'); // Access the CurrentDay ID 
+
+    currentDayEl.text (moment().format('LLLL')); // Targets the CurrentDay Element and replaces the Text with the Time and Date in the format Day, Month Date, Year Time (AM/PM)
+
+//
+
+
+
+
